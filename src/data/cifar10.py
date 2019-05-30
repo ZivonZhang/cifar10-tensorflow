1a8a3ec4671d31958f1ce4ff20020583d67770b2
lab226 <466170265@qq.com>
2019-05-30 14:46:16 +0800

    123456

--- src/data/cifar10.py
+++ src/data/cifar10.py
@@ -9,7 +9,7 @@ import cv2
 class Corpus:
     
     def __init__(self):
-        self.load_cifar10('data/CIFAR10_data')
+        self.load_cifar10('data/cifar-10-batches-py')
         self._split_train_valid(valid_rate=0.9)
         self.n_train = self.train_images.shape[0]
         self.n_valid = self.valid_images.shape[0]
@@ -29,7 +29,7 @@ class Corpus:
                 if 'Windows' in platform.platform():
                     cifar10 = pickle.load(fo, encoding='bytes')
                 elif 'Linux' in platform.platform():
-                    cifar10 = pickle.load(fo)
+                    cifar10 = pickle.load(fo,encoding='bytes')
             for i in range(len(cifar10[b"labels"])):
                 image = numpy.reshape(cifar10[b"data"][i], (3, 32, 32))
                 image = numpy.transpose(image, (1, 2, 0))
@@ -47,7 +47,7 @@ class Corpus:
                 if 'Windows' in platform.platform():
                     cifar10 = pickle.load(fo, encoding='bytes')
                 elif 'Linux' in platform.platform():
-                    cifar10 = pickle.load(fo)
+                    cifar10 = pickle.load(fo, encoding='bytes')
             for i in range(len(cifar10[b"labels"])):
                 image = numpy.reshape(cifar10[b"data"][i], (3, 32, 32))
                 image = numpy.transpose(image, (1, 2, 0))
